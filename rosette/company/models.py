e741d91f05c685e61472bdbeb8dd8df15619877f
aputzolu <anthony.putzolu@gmail.com>
2017-06-30 16:54:45 +0200

    Add Models

--- rosette/company/models.py
+++ rosette/company/models.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
-
 class Company(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        """ 
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que 
+        nous traiterons plus tard et dans l'administration
+        """
+        return self.name
+
+class User(models.Model):
    name = models.CharField(max_length=100)
+   company = models.ForeignKey('Company')
+
+   def __str__(self):
+        return self.name
--- rosette/user/models.py
+++ rosette/user/models.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from company.models import Company
 
 # Create your models here.
 
-class User(models.Model):
-   name = models.CharField(max_length=100)
+# class User(models.Model):
+#    name = models.CharField(max_length=100)
+#    company = models.ForeignKey('company.Company')
+
+#    def __str__(self):
+#         return self.name
